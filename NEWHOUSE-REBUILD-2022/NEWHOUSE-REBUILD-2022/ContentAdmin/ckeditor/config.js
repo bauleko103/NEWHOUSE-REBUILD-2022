c6d00c9164bcb9becdcd7ea9d0a2bf055900dc14
bauleko103 <59721895+bauleko103@users.noreply.github.com>
2022-09-29 06:42:18 +0700

    baule_slide

--- NEWHOUSE-REBUILD-2022/NEWHOUSE-REBUILD-2022/ContentAdmin/ckeditor/config.js
+++ NEWHOUSE-REBUILD-2022/NEWHOUSE-REBUILD-2022/ContentAdmin/ckeditor/config.js
@@ -7,12 +7,12 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
     // config.uiColor = '#AADC6E';
-    config.filebrowserBrowseUrl = "/ContentWebmaster/ckfinder/ckfinder.html";
-    config.filebrowserImageUrl = "/ContentWebmaster/ckfinder/ckfinder.html?type=Images";
-    config.filebrowserFlashUrl = "/ContentWebmaster/ckfinder/ckfinder.html?type=Flash";
-    config.filebrowserUploadUrl = "/ContentWebmaster/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files";
-    config.filebrowserImageUploadUrl = "/ContentWebmaster/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images";
-    config.filebrowserFlashUploadUrl = "/ContentWebmaster/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash";
+    config.filebrowserBrowseUrl = "/ContentAdmin/ckfinder/ckfinder.html";
+    config.filebrowserImageUrl = "/ContentAdmin/ckfinder/ckfinder.html?type=Images";
+    config.filebrowserFlashUrl = "/ContentAdmin/ckfinder/ckfinder.html?type=Flash";
+    config.filebrowserUploadUrl = "/ContentAdmin/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files";
+    config.filebrowserImageUploadUrl = "/Data";
+    config.filebrowserFlashUploadUrl = "/ContentAdmin/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash";
  
     config.extraPlugins = 'youtube';
     config.youtube_responsive = true;
