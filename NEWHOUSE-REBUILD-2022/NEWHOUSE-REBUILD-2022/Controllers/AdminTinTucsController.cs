aa5572b5812ce7c4cd0e2d40230e43c8ca7f7501
bauleko103 <4501104017@student.hcmue.edu.vn>
2022-10-02 15:31:05 +0700

    baule_up_tintuc

---
+++ NEWHOUSE-REBUILD-2022/NEWHOUSE-REBUILD-2022/Controllers/AdminTinTucsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NEWHOUSE_REBUILD_2022.Models;
+
+namespace NEWHOUSE_REBUILD_2022.Controllers
+{
+    public class AdminTinTucsController : Controller
+    {
+        private NEWHOUSE2022Entities db = new NEWHOUSE2022Entities();
+
+        // GET: AdminTinTucs
+        public ActionResult Index()
+        {
+            return View(db.TinTucs.ToList());
+        }
+
+      
+        // GET: AdminTinTucs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminTinTucs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TinTuc tinTuc, HttpPostedFileBase uploadhinh)
+        {
+            db.TinTucs.Add(tinTuc);
+            db.SaveChanges();
+            if (uploadhinh != null && uploadhinh.ContentLength > 0)
+            {
+                int id = int.Parse(db.TinTucs.ToList().Last().ID.ToString());
+
+                string _FileName = "";
+                int index = uploadhinh.FileName.IndexOf('.');
+                _FileName = "tintuc" + id.ToString() + "." + uploadhinh.FileName.Substring(index + 1);
+                string _path = Path.Combine(Server.MapPath("~/Content/img/tintuc/"), _FileName);
+                uploadhinh.SaveAs(_path);
+
+                TinTuc unv = db.TinTucs.FirstOrDefault(x => x.ID == id);
+                unv.Hinh = _FileName;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "AdminTinTucs");
+
+           
+        }
+         
+        // GET: AdminTinTucs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TinTuc tinTuc = db.TinTucs.Find(id);
+            if (tinTuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tinTuc);
+        }
+
+        // POST: AdminTinTucs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit( TinTuc tinTuc, HttpPostedFileBase uploadhinh)
+        {
+            TinTuc unv = db.TinTucs.FirstOrDefault(x => x.ID == tinTuc.ID);
+            unv.TuaDe = tinTuc.TuaDe;
+            unv.Link = tinTuc.Link;
+            if (uploadhinh != null && uploadhinh.ContentLength > 0)
+            {
+                int id = tinTuc.ID;
+                string _FileName = "";
+                int index = uploadhinh.FileName.IndexOf('.');
+                _FileName = "tintuc" + id.ToString() + "." + uploadhinh.FileName.Substring(index + 1);
+                string _path = Path.Combine(Server.MapPath("~/Content/img/tintuc"), _FileName);
+                uploadhinh.SaveAs(_path);
+                unv.Hinh = _FileName;
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+         
+
+        // GET: AdminTinTucs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TinTuc tinTuc = db.TinTucs.Find(id);
+            if (tinTuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tinTuc);
+        }
+
+        // POST: AdminTinTucs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TinTuc tinTuc = db.TinTucs.Find(id);
+            db.TinTucs.Remove(tinTuc);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
